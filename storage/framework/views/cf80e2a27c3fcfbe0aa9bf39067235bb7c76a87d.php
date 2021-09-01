edaad6faa1d5d512bacc9598e4362c44f32c4b8b
nightfall <68837055+rolyquellca2020@users.noreply.github.com>
2021-09-01 10:33:32 -0500

    Add files via upload

---
+++ storage/framework/views/cf80e2a27c3fcfbe0aa9bf39067235bb7c76a87d.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="icon" type="image/png" sizes="96x96" href="/img/favicon.png">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+    <title>sistema de control del personal</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+            <!-- CSRF Token -->
+    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
+    <!--  Fonts and icons     -->
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic">
+    <link rel="stylesheet" href="//fonts.googleapis.com/icon?family=Material+Icons">
+    <link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+
+  <body>
+    <div id="app"></div>
+    <script type="text/javascript" src="<?php echo e(mix('js/main.js')); ?>"></script>
+    <script type="text/javascript" src="<?php echo e(asset('js/validarRUT.js')); ?>"></script>
+  </body>
+</html>
